e9c63635d1f29c5701f927c5f5444af019239723
nativeborncitizen@gmail.com <nativeborncitizen@gmail.com@4d175d60-8fdb-afce-c0be-e81b618dfcf7>
2012-01-25 22:42:59 +0000

    Fixing errors with wrong year. List of calendars now forms on base of directory content.

---
+++ trunk/CalendarLocator.py
@@ -0,0 +1,9 @@
+import ConfigParser, os
+
+def getCalendarFilenamesFromConfig(configFile,  open = open):
+    Config = ConfigParser.ConfigParser()
+    Config.readfp(open(configFile, 'r'))
+    return Config.get('Calendar',  'calendars').split(',')
+    
+def getCalendarFilenamesFromDir(dir):
+    return map(lambda s: os.path.join('xml', s), filter(lambda s: s[-3:] == 'xml', os.listdir(dir)))
--- trunk/cc.py
+++ trunk/cc.py
@@ -1,24 +1,56 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import Easter,  CalendarReader, ConsoleVisualizer,  RightDate, sys,  datetime
+import Easter,  CalendarReader, ConsoleVisualizer, CalendarLocator, RightDate
+import sys, datetime, getopt,  functools
 
-CONFIG_FILE = 'cc.ini'
+def usage():
+    print """cc.py [-c|-f config_file|-d dir_name] [date]
+
+c - список календарей получить из файла cc.ini;
+f config_file - список календарей получить из файла с именем config_file;
+d dir_name - в качестве списка календарей использовать все xml-файлы из каталога dir_name;
+без параметров - по умолчанию -d xml
+date по умолчанию сегодняшняя"""
+    sys.exit(2)
 
 d = datetime.date.today()
+getCalendarFilenames = functools.partial(CalendarLocator.getCalendarFilenamesFromDir, 'xml')
 
-if len(sys.argv) > 1:
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "cf:d:")
+except getopt.GetoptError, err:
+    usage()
+
+if len(opts) != 1:
+    usage()
+
+o, a = opts[0]
+
+if o == "-c":
+    getCalendarFilenames = functools.partial(CalendarLocator.getCalendarFilenamesFromConfig, 'cc.ini')
+elif o == "-f":
+    getCalendarFilenames = functools.partial(CalendarLocator.getCalendarFilenamesFromConfig, a)
+elif o == "-d":
+    getCalendarFilenames = functools.partial(CalendarLocator.getCalendarFilenamesFromDir, a)
+else:
+    print "%s - ошибочная опция\n" % o
+
+if len(args) == 1:
     try:
-        (dd,  mm,  yy) = sys.argv[1].split('.')
-        d = datetime.date(int(yy), int(mm), int(dd))
-    except:
-        print "Неверная дата"
+        d = datetime.datetime.strptime(args[0], '%d.%m.%Y').date()
+        if d.year < 1900:
+            raise ValueError
+    except ValueError:
+        print "Неверная дата\n"
+        usage()
     
 CV = ConsoleVisualizer.ConsoleVisualizer()
 
-print Easter.dateToReadableStr(d)
+print "%s, %s" % (Easter.getWeekdayStr(d), Easter.dateToReadableStr(d))
 print "(%s ст. ст.)" % Easter.dateToReadableStr(Easter.newToOldStyle(d))
-for filename in CalendarReader.getCalendarFilenames(CONFIG_FILE):
+
+for filename in getCalendarFilenames():
     try:
         CalendarReader.parseCalendar(filename, RightDate.RightDate(d),  CV)
     except CalendarReader.CalendarFileError:
