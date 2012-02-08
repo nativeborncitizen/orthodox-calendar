77f71642b33303294cf6fa601cbe93c8c49de7c0
nativeborncitizen@gmail.com <nativeborncitizen@gmail.com@4d175d60-8fdb-afce-c0be-e81b618dfcf7>
2012-02-08 00:51:41 +0000

    PEP8 adopting

--- trunk/CalendarLocator.py
+++ trunk/CalendarLocator.py
@@ -1,9 +1,13 @@
-import ConfigParser, os
+import os
+
+import ConfigParser
 
 def getCalendarFilenamesFromConfig(configFile,  open = open):
     Config = ConfigParser.ConfigParser()
     Config.readfp(open(configFile, 'r'))
     return Config.get('Calendar',  'calendars').split(',')
-    
+
+
 def getCalendarFilenamesFromDir(dir):
-    return map(lambda s: os.path.join('xml', s), filter(lambda s: s[-3:] == 'xml', os.listdir(dir)))
+    return map(lambda s: os.path.join('xml', s),
+               filter(lambda s: s[-3:] == 'xml', os.listdir(dir)))
