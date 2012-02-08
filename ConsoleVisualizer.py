f1352019211bf313e92fb7c51c250e4dc336c18d
nativeborncitizen@gmail.com <nativeborncitizen@gmail.com@4d175d60-8fdb-afce-c0be-e81b618dfcf7>
2012-02-08 21:50:11 +0000

    Visualizer was removed

--- ConsoleVisualizer.py
+++ ConsoleVisualizer.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import Visualizer
 
-
-class ConsoleVisualizer(Visualizer.Visualizer):
+class ConsoleVisualizer:
     """
     Класс для отображения информации в консоли
     """
+    def __init__(self):
+        self._s = []
+
+    def add(self, text,  score):
+        """
+        Добавление новой строки
+        вход: строка
+        вход: вес для сортировки
+        """
+        self._s.append((text,  score))
+
+    def clear(self):
+        """
+        Очистить содержимое буфера
+        """
+        self._s = []
 
     def __str__(self):
         """
